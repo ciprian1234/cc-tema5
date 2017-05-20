e2519ef711312bd58c85eb2c23878354d64429cd
AndreiCiprian <andrei.stefanel.ciprian@gmail.com>
2017-05-20 16:55:49 +0300

    App created

---
+++ app.js
@@ -0,0 +1,21 @@
+var express = require('express');
+var app = express();
+
+//config web app
+app.set('view engine', 'ejs');
+app.set('views', __dirname+'/public');
+
+
+//Use middleware
+app.use(express.static('public'));
+
+
+var server = app.listen(80, function(){
+	var host = server.address().address;
+	var port = server.address().port;
+	console.log("Web app listening at http://%s:%s", host, port);
+});
+
+app.get('/', function(req, res){
+	res.render('index')
+});
